f671319b335fb48ad2edefe722a326c2abfb0bbb
kabsyaks kabeer <36848823+kabsyakskabeer@users.noreply.github.com>
2023-02-05 11:09:33 +0100

    sw

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,11 @@
-
-
-    self.addEventListener("fetch", (event) => {
-        event.respondWith("Welcome to the Gotham Imperial Hotel.\n"+
-        "There seems to be a problem with your connection.\n"+
-        "We look forward to telling you about our hotel as soon as you go online.");
-      });
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+  fetch(event.request).catch(function() {
+  return new Response(
+  "Welcome to the Gotham Imperial Hotel.\n"+
+  "There seems to be a problem with your connection.\n"+
+  "We look forward to telling you about our hotel as soon as you go online."
+  );
+  })
+  );
+  });
